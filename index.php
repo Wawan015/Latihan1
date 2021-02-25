71047d0937e0170a273b80860f51d7786a280416
Wawan <pendekarcoeg03@gmail.com>
2021-02-26 01:05:01 +0800

    tugas

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Beranda Saya </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div class="container">    
+        <div class="navbar" style="background-image: url('media/bgbatik.jpg'); color:ghostwhite;">
+        <div class="pull-left">
+        <img src="media/android-logo-12426.png" width="100">
+        </div>
+        <div class="pull-left">
+        <h1> W1 Ponsel</h1>
+        <p>Melayani Sepenuh Hati</p>
+        </div>
+         </div>
+        <div class="menu">
+            <div class="bg"style="background-color: red; margin:2px;">
+                <a href="#" style="padding-left : 10px; color: black;"> Beranda</a>
+                <a href="#" style="padding-left : 10px; color: black;"> Alamat</a>
+                <a href="#" style="padding-left : 10px; color: black;"> Layanan</a>
+                <a href="#" style="padding-left : 10px; color: black;"> Fitur</a>
+                
+            </div>
+        </div>
+   
+        <div class="row">
+            <div class="sidebar">
+            <div class="col-sm-4">
+                <div><img src="media/Teknisi.jpg" width="500"></div>
+                <h3>Pengerjaan dengan alat - alat lengkap</h3>
+                <p>Alat yang kami miliki lengkap dan selalu mengikuti perkembangan terbaru Hal Tersebut tidak dan tidak bukan demi user kami agar selalu mendapat kepuasan</p>  
+                <div class="col-sm-4" style="float:right; width:20%; " >
+                          
+            </div>           
+            </div>
+                       
+            <div class="col-md-4" style="float:right; width:20%; " >
+                <div class="widget" style="background-color: aliceblue; color:black;">
+                <h3>TIPS & TRIK</h3>
+                <ul class="menusidebar"></ul>
+                <li><a href=" ">cara menjaga keamanan akun WA </a></li>
+                <li><a href=" ">tips menjaga keawetan baterai HP</a></li>                
+            </div>
+            </div>
+            </div> 
+        </div>
+    </div>
+</body>
+
+</html>
